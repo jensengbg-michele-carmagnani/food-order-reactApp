4eea624529bd758549e4a24581c19c777a8d0471
Michele Carmagnani <michele.carmagnani@gmail.com>
2021-07-18 20:38:23 +0200

    create HeaderCardButton.js

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,13 @@
 import React from "react";
 import classes from "./Header.module.css"
 import mealsImage from "../../assets/meals.jpg"
-
+import HeaderCardButton from './HeaderCardButton'
 const Header = (props) => {
   return (
     <React.Fragment>
       <header className={classes.header}>
         <h1>App order meals</h1>
-        <button>Cart </button>
+        <HeaderCardButton/>
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt="A table full of deliious food!"></img>
