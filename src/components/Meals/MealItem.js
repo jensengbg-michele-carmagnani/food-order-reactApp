095eee843401e0c6c90463584fa388f851d7f2f5
Michele Carmagnani <michele.carmagnani@gmail.com>
2021-07-19 08:52:50 +0200

    add MealItemForm.js && Button.js && Input.js

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import MealItemForm from './MealItemForm'
 import classes from "./MealItem.module.css";
 const MealItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
@@ -6,10 +7,10 @@ const MealItem = (props) => {
     <li className={classes.meal}>
       <div>
         <h3>{props.name}</h3>
-      </div>
       <div className={classes.description}> {props.description}</div>
       <div className={classes.price}>{price}</div>
-      <div></div>
+      </div>
+      <div><MealItemForm/></div>
     </li>
   );
 };
--- src/components/Meals/MealItemForm.js
+++ src/components/Meals/MealItemForm.js
@@ -1,11 +1,21 @@
-import React from 'react'
-
+import React from "react";
+import Input from '../UI/Input'
+import Button from '../UI/Button'
+import classes from "./MealItem.module.css";
 const MealItemForm = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <form className={classes.form}>
+      <Input lable="Amaount" input={{
+        id: 'amount',
+        type: 'number',
+        min: '1',
+        max: '5',
+        step: '1',
+        defaultValue: '1'
+      }} />
+      <Button>+ Add</Button>
+    </form>
+  );
+};
 
-export default MealItemForm
+export default MealItemForm;
