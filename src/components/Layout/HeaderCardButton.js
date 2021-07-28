1efdb26b97b8e5eb11eed5dbac36636fb500d5a6
Michele Carmagnani <michele.carmagnani@gmail.com>
2021-07-28 09:40:07 +0200

    add bumb to the headerCardButton.js

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -1,17 +1,44 @@
 import React from "react";
-import {useContext} from "react"
+import {useContext, useEffect, useState } from "react"
 import CartContext from "../../store/cart-context"
 import CartIcon from "../Cart/CartIcon";
 import classes from './HeaderCardButton.module.css'
+
+
 const HeaderCardButton = (props) => {
 const cartCtx = useContext(CartContext)
-const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) =>{
+const {items} = cartCtx
+
+const numberOfCartItems = items.reduce((currentNumber, item) =>{
   return currentNumber + item.amount;
 }, 0);
 console.log('ctxCart ', numberOfCartItems)
 
+const [btnIsHighlighted , setBtnIsHighlighted ] = useState(false)
+
+const btnClass = `${classes.button} ${btnIsHighlighted ? classes.bump : ""}`
+
+
+
+useEffect(() =>{
+  if(cartCtx.items.length === 0){
+    return;
+  }
+  setBtnIsHighlighted(true)
+
+  const timer = setTimeout(()=>{
+    setBtnIsHighlighted(false)
+  },300)
+
+  //clean up function 
+  return () => {
+    clearTimeout(timer)
+  }
+
+}, [items])
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClass} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
